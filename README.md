4bd57b10c42413da2b5351c65d860ea6347251f8
Riley Lundquist <rlundquist3@gmail.com>
2016-05-12 18:27:05 -0400

    reinitialized

---
+++ README.md
@@ -0,0 +1,8 @@
+# colony-farm-site
+
+Work-in-progress website work for Colony Farm (colony.farm)
+
+Basic Node.js version running May 12, 2016. May need to add Python for logging work.
+
+### Oddities when running
+* proc command should be `node` on remote, but `nodejs` locally
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+// app.post('/contact', function(request, response) {
+//     var options, transporter;
+//
+//     transporter = nodemailer.createTransport('SMTP', {
+//         service: 'GandiMail',
+//         auth: {
+//             user: 'noreply@colony.farm',
+//             pass: 'crickets123$%'
+//         }
+//     });
+//
+//     options = {
+//         from: request.body.name + ' ' + req.body.email,
+//         to: 'info@colony.farm',
+//         subject: 'Website contact form',
+//         text: req.body.message
+//     };
+//
+//     transporter.sendMail(options, function(error, response) {
+//         if (error) {
+//
+//         } else {
+//
+//         }
+//     });
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
