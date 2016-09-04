4c3716613b7e35ed339d0b8df865ee702549c87d
Riley Lundquist <rlundquist3@gmail.com>
2016-09-04 10:09:48 -0700

    converting to pug

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
   response.render('index');
