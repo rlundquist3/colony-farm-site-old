f06b506c84bab1580899ce4d2cae985bda23c124
Riley Lundquist <rlundquist3@gmail.com>
2016-09-04 16:47:12 -0700

    set up firebase

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
+var firebase = require('firebase');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,8 +11,38 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-app.get('/', function(request, response) {
-  response.render('index');
+// Initialize the app with no authentication
+firebase.initializeApp({
+  databaseURL: "https://colonyfarm-bae40.firebaseio.com"
+});
+
+// The app only has access to public data as defined in the Security Rules
+var db = firebase.database();
+var ref = db.ref("/some_public_resource");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/blog/:title?', function(req, res) {
+    var postsRef = db.ref('posts');
+    // postsRef.orderByChild('date').limitToLast(5).on('child_added', function(snapshot) {
+    //     console.log(snapshot.key);
+    // });
+    postsRef.once('value', function(data) {
+        var postList = Object.keys(data).map(function(value) {
+							         return data[value]})
+    });
+    if (title === undefined) {
+		res.status(503);
+		res.render('blog', {posts: postList});
+	} else {
+		var post = posts[title] || {};
+		res.render('post', {post: post});
+	}
 });
 
 // app.post('/contact', function(request, response) {
